927dd45cec6c3b114a598bfa4be839b9917d4820
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-03 21:50:34 +0100

    adjusted font size and text

--- src/components/CVPage.vue
+++ src/components/CVPage.vue
@@ -10,7 +10,7 @@
             </div>
             <div v-else class="h-[120px] xl:h-[84px]"></div>
             <table class="grid place-items-center my-12">
-                <tr class="bg-gray-800 text-xl xl:text-2xl text-gray-200 text-center font-mono font-bold">
+                <tr class="bg-gray-800 text-lg xl:text-2xl text-gray-200 text-center font-mono font-bold">
                     <td @mouseover="onHoverBachelor(eduEntries[1])" @mouseleave="mouseOff(eduEntries[1])" class="w-20 h-20 xl:h-52 xl:w-52 m-6 hover:bg-blue-200 hover:text-gray-800 hover:cursor-default">2015</td>
                     <td @mouseover="onHoverBachelor(eduEntries[1])" @mouseleave="mouseOff(eduEntries[1])" class="w-20 h-20 xl:h-52 xl:w-52 m-6 hover:bg-blue-200 hover:text-gray-800 hover:cursor-default">2016</td>
                     <td @mouseover="onHoverBachelor(eduEntries[1])" @mouseleave="mouseOff(eduEntries[1])" class="w-20 h-20 xl:h-52 xl:w-52 m-6 hover:bg-blue-200 hover:text-gray-800 hover:cursor-default">2017</td>
@@ -39,7 +39,7 @@ export default {
         return {
             eduEntries: [
                 {Title: "Master's degree in molecular biology.", Details: "Automation of high throughput image analysis techniques in microscopy.", hover: false},
-                {Title: "Bachelor's degree in biology", Details: "Molecular and computational biology", hover: false}],
+                {Title: "Bachelor's degree in biology", Details: "Molecular and computational biology.", hover: false}],
             jobEntries: [
                 {Title: "Doctoral candidate in computational biology.", Details: "Computational analysis of gene regulation in development and disease.", hover: false},
                 {Title: "Scientific assistant in Python programming.", Details: "Making curriculum and coding challenges in Python for a course in computational biology.", hover: false},
