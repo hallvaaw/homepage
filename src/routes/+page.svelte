f979c91606b0ed501280255a33c43d9251359b07
hallvaaw <hallvard_wahler@hotmail.com>
2023-03-31 23:07:28 +0200

    rewrite homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="container">
+    <h1>Hallvard A. Wæhler</h1>
+    <p>I am a bioinformatician and software developer.</p>
+    <p>I am involved in research projects focusing on biological image analysis and genetics. My main tasks include writing data pipelines, making desktop applications, statistical analysis and front end development.</p>
+    <h5>Main technologies:</h5>
+    <ul>
+       <li>Python</li>
+       <li>R</li>
+       <li>Vue</li>
+       <li>Svelte</li>
+       <li>Linux</li>
+    </ul>
+    <h3>Current projects:</h3>
+    <p>ANDA: A desktop application for automated image analysis made with Tauri.</p>
+    <p>ShinyArchRUiO: A platform for visualizing genetic analyses at single cell resolution written in R.</p>
+</main>
+<footer class="container">
+    <h3>Contact</h3>
+    <h6 style="margin-bottom: 6px;"><a href="https://github.com/hallvaaw" target="_blank" rel="noopener noreferrer">GitHub</a></h6>
+    <h6><a href="https://www.linkedin.com/in/hallvard-austin-wæhler/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h6>
+</footer>
