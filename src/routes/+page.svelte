66c5b650110bef93079fcfa31899b9a3a65fa745
hallvaaw <hallvard_wahler@hotmail.com>
2023-03-31 23:17:01 +0200

    rewrite homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,3 +19,4 @@
     <h6 style="margin-bottom: 6px;"><a href="https://github.com/hallvaaw" target="_blank" rel="noopener noreferrer">GitHub</a></h6>
     <h6><a href="https://www.linkedin.com/in/hallvard-austin-wæhler/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h6>
 </footer>
+
