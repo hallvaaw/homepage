dde3631e7b532aff4358684723754314c4ba886c
hallvaaw <hallvard_wahler@hotmail.com>
2023-04-22 20:10:15 +0200

    adjusted margins

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <main class="container">
-        <h1>Hallvard A. Wæhler</h1>
-        <p>I am a bioinformatician and software developer.</p>
+        <h1 styl>Hallvard A. Wæhler</h1>
+        <p>I am a bioinformatician and software developer at the University of Oslo.</p>
         <p>I am involved in research projects focusing on biological image analysis and genetics. My main tasks include writing data pipelines, making desktop applications, statistical analysis and front end development.</p>
         <h5>Main technologies:</h5>
         <ul style="margin-left: 26px;">
@@ -11,12 +11,12 @@
            <li>Tauri</li>
            <li>Linux</li>
         </ul>
-        <h3>Current projects:</h3>
+        <h3 style="margin-bottom: 20px;">Current projects:</h3>
         <p>ANDA: A desktop application for automated image analysis made with Tauri.</p>
         <p>ShinyArchRUiO: A platform for visualizing genetic analyses at single cell resolution written in R.</p>
     </main>
     <footer class="container">
-        <h3>Contact</h3>
+        <h3 style="margin-bottom: 20px;">Contact</h3>
         <h6 style="margin-bottom: 6px;"><a href="https://github.com/hallvaaw" target="_blank" rel="noopener noreferrer">GitHub</a></h6>
         <h6><a href="https://www.linkedin.com/in/hallvard-austin-wæhler/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h6>
     </footer>
