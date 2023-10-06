91eb33f519df7426840a3a74ed31ef6813c96458
hallvaaw <hallvard_wahler@hotmail.com>
2023-10-06 12:08:32 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
     <main class="container">
         <h1>Hallvard A. Wæhler</h1>
-        <p>I am a bioinformatician and software developer at the University of Oslo.</p>
-        <p>I am involved in research projects focusing on biological image analysis and genetics. My main tasks include writing data pipelines, making desktop applications, statistical analysis and front end development.</p>
         <h5>Favorite technologies:</h5>
         <ul style="margin-left: 26px;">
            <li>Python</li>
@@ -14,7 +12,6 @@
         <h3 style="margin-bottom: 20px;">Current projects:</h3>
         <p style="margin-bottom: 16px;">ANDA: A desktop application for automated image analysis of neuronal cells.</p>
         <p style="margin-bottom: 16px;">pydrc: A Python library for dose-response models.</p>
-        <p>LINE mine: A web application for visualizing LINE-1 transcripts.</p>
     </main>
     <footer class="container">
         <h6 style="margin-bottom: 6px;"><a href="https://github.com/hallvaaw" target="_blank" rel="noopener noreferrer">GitHub</a></h6>
