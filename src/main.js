4a90cd6da6a9a5211028d4be8a349da203204ecb
hallvaaw <hallvard_wahler@hotmail.com>
2024-09-17 15:21:55 +0200

    update css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import '../node_modules/@picocss/pico/css/pico.min.css'
+import '../node_modules/chota/src/chota.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
