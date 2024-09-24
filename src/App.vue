c1189dddce9b63fb11b4b1ed7837f2d6bc60aa53
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2024-09-24 15:08:38 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <template>
     <main class="container">
         <h1>Hallvard Wæhler</h1>
-        <h5>Favorite technologies:</h5>
-        <ul style="margin-left: 20px;">
-           <li>Python</li>
-           <li>Vue</li>
-           <li>Tauri</li>
-           <li>Linux</li>
-           <li>Julia</li>
-        </ul>
-        <h3 style="margin-bottom: 20px;">Current projects:</h3>
-        <p style="margin-bottom: 16px;">ANDA: A desktop application for automated image analysis of neuronal cells.</p>
-        <p style="margin-bottom: 16px;">pydrc: A Python library for dose-response models.</p>
     </main>
     <footer class="container">
         <h6 style="margin-bottom: 6px;"><a href="https://github.com/hallvaaw" target="_blank" rel="noopener noreferrer">GitHub</a></h6>
