f7eb3b46c7e60ec7121e6b45b2c23ba6e8ca3829
hallvaaw <hallvard_wahler@hotmail.com>
2024-09-27 13:51:22 +0200

    add bikes

--- src/App.vue
+++ src/App.vue
@@ -22,20 +22,26 @@ export default {
     },
     methods: {
         async getWeather() {
-               const res = await fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.9369&lon=10.7600')
-               const data = await res.json()
-               console.log(data.properties.timeseries[0].data.next_6_hours.summary)
+            const res = await fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.9369&lon=10.7600')
+            const data = await res.json()
+            console.log(data.properties.timeseries[0].data.next_6_hours.summary)
         },
         async getBike() {
-               const res = await fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json')
-               const data = await res.json()
-               console.log("Bjølsen studentby:", data.data.stations[175].num_bikes_available)
-               console.log("Badebakken:", data.data.stations[66].num_bikes_available)
-               console.log("Stavangergata:", data.data.stations[84].num_bikes_available)
-               console.log("Tåsenløkka:", data.data.stations[118].num_bikes_available)
-               console.log("Bentsebrugata:", data.data.stations[147].num_bikes_available)
-               console.log("Bjølsendumpa:", data.data.stations[195].num_bikes_available)
-               console.log("Myraløkka Øst: ", data.data.stations[117].num_bikes_available)
+            const res = await fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json')
+            const data = await res.json()
+            const stasjoner = {
+                "Bjølsen studenby": 175,
+                "Badebakken": 66,
+                "Stavangergata": 84,
+                "Tåsenløkka": 118,
+                "Bentsebrugata": 147,
+                "Bjølsendumpa": 195,
+                "Myraløkka Øst": 117
+            } 
+            let entries = Object.entries(stasjoner)
+            entries.forEach(function([key, value]) {
+                console.log(key, data.data.stations[value].num_bikes_available)
+            })
         }
     }
 }
