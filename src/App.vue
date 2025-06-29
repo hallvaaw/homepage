71a939724499646b71abc9a2bb8e63541b4ee541
hallvaaw <hallvard_wahler@hotmail.com>
2025-06-29 16:38:40 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,7 @@ export default {
         }
     },
     mounted() {
-        this.getWeather(),
-        this.getBike()
+        this.getWeather()
     },
     methods: {
         async getWeather() {
@@ -45,51 +44,6 @@ export default {
             const data = await res.json()
             console.log(data.properties.timeseries[0].data.next_6_hours.summary)
         },
-        async getBike() {
-            const res = await fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json')
-            const data = await res.json()
-            const stasjoner = {
-                "Bjølsen studentby": {
-                    available_bikes: data.data.stations[175].num_bikes_available, color_code: this.setColor(data.data.stations[175].num_bikes_available)
-                },
-                "Badebakken": {
-                    available_bikes: data.data.stations[66].num_bikes_available, color_code: this.setColor(data.data.stations[66].num_bikes_available)
-                },
-                "Stavangergata": {
-                    available_bikes: data.data.stations[84].num_bikes_available, color_code: this.setColor(data.data.stations[84].num_bikes_available)
-                },
-                "Tåsenløkka": {
-                    available_bikes: data.data.stations[118].num_bikes_available, color_code: this.setColor(data.data.stations[118].num_bikes_available)
-                },
-                "Bentsebrugata": {
-                    available_bikes: data.data.stations[147].num_bikes_available, color_code: this.setColor(data.data.stations[147].num_bikes_available)
-                },
-                "Bjølsendumpa": {
-                    available_bikes: data.data.stations[195].num_bikes_available, color_code: this.setColor(data.data.stations[195].num_bikes_available)
-                },
-                "Myraløkka Øst": {
-                    available_bikes: data.data.stations[117].num_bikes_available, color_code: this.setColor(data.data.stations[117].num_bikes_available)
-                },
-                "Sagene bussholdeplass": {
-                    available_bikes: data.data.stations[141].num_bikes_available, color_code: this.setColor(data.data.stations[141].num_bikes_available)
-                },
-                "Arendalsgata": {
-                    available_bikes: data.data.stations[156].num_bikes_available, color_code: this.setColor(data.data.stations[156].num_bikes_available)
-                }
-            } 
-            this.myDictionary = stasjoner
-        },
-        setColor(num_bikes) {
-            if (num_bikes >= 10) {
-                return "#28a745";
-            } else if (num_bikes > 5 && num_bikes < 10) {
-                return "#d4d700";
-            } else if (num_bikes > 0 && num_bikes < 6) {
-                return "#ffc107";
-            } else {
-                return "#dc3545";
-            }
-        }
     }
 }
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
